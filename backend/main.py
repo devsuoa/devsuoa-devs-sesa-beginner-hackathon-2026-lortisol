b9c1ec1893c948c884a9b4b338516116aa17c69e
jzchenn <jackyc.pata@gmail.com>
2026-04-18 12:02:53 +1200

    Setup backend folder

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Essential for React communication
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"], # Vite default port
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/health")
+def health_check():
+    return {"status": "ok", "message": "Backend is reachable! Testing successful"}
